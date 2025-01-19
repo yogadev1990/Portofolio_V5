c47e845b109f5b9eee6298ab4bed548af2f5dcd5
Randa Yoga Saputra <04031382328073@student.unsri.ac.id>
2025-01-20 00:53:20 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -32,9 +32,9 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
             <center>
               <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-                © 2023{" "}
+                © 2025{" "}
                 <a href="https://flowbite.com/" className="hover:underline">
-                  EkiZR™
+                  Revanda™
                 </a>
                 . All Rights Reserved.
               </span>
@@ -53,9 +53,9 @@ const ProjectPageLayout = () => (
       <center>
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-          © 2023{" "}
+          © 2025{" "}
           <a href="https://flowbite.com/" className="hover:underline">
-            EkiZR™
+            Revanda™
           </a>
           . All Rights Reserved.
         </span>
--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -110,7 +110,7 @@ const ProjectDetails = () => {
         ...selectedProject,
         Features: selectedProject.Features || [],
         TechStack: selectedProject.TechStack || [],
-        Github: selectedProject.Github || 'https://github.com/EkiZR',
+        Github: selectedProject.Github || 'https://github.com/yogadev1990',
       };
       setProject(enhancedProject);
     }
